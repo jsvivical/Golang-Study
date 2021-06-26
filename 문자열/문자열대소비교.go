75eb64bc0f10581a2c63a71ffe36a48347d6df91
jsvivical <jsvivical@gmail.com>
2021-06-26 14:36:47 +0900

    깃 연습용으로 수정한 파일

--- 문자열/문자열대소비교.go
+++ 문자열/문자열대소비교.go
@@ -7,8 +7,9 @@ func main() {
 	str2 := "aaaaAAAA"
 	str3 := "BBAD"
 	str4 := "ZZZ"
-
+	str5 := "git 연습용"
 	fmt.Printf("%s > %s : %v\n", str1, str2, str1 > str2)
 	fmt.Printf("%s < %s : %v\n", str1, str3, str1 < str3)
 	fmt.Printf("%s <= %s : %v\n", str1, str4, str1 <= str4)
+	fmt.Println(str5)
 }
